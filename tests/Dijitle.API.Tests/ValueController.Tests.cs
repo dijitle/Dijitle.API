cde1496f01cd2764050c1ca6cfda2fac295ccb08
Arunas Slepikas <dijitle@gmail.com>
2018-11-11 11:49:39 -0600

    adding tests project

---
+++ tests/Dijitle.API.Tests/ValueController.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using Dijitle.API.Controllers;
+
+namespace Dijitle.API.Tests
+{
+    [TestFixture, Category("UnitTests")]
+    public class ValueControllerTests
+    {
+        private ValuesController _vc;
+
+        [SetUp]
+        public void Setup()
+        {
+            _vc = new ValuesController();
+        }
+
+        [Test]
+        public void TestGet()
+        {
+            //arrage
+            string[] expected = new string[] { "value1", "value2" };
+            string[] actual = (string[])_vc.Get().Value;
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _vc = null;
+        }
+    }
+}
